129ae1eec3a2790104a2cc5563a5a0b7d23982d3
Gabriel Diaz <gabidiaz4231@gmail.com>
2026-06-06 17:24:54 -0300

    Configuracion de cloudinary

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -23,8 +23,14 @@ import { Public } from '../common/decorators/public.decorator';
 import { GetUserId } from '../common/decorators/getUserId.decorator';
 import { Roles } from '../common/decorators/permission.decorator';
 import { UserRole } from '../common/enums/user-role.enum';
-import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 @Controller('products')
 export class ProductController {
